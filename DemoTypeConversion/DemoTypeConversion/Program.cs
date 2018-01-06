7fac9398b3738c0d13abceb99089cbe305695892
trainsOE2 <meghasingh.ms@gmail.com>
2018-01-06 07:46:57 +0530

    DemoTypeConversion corrected

---
+++ DemoTypeConversion/DemoTypeConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "1234";
+            int i = Convert.ToInt32(s);
+            int j = int.Parse(s);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+        }
+    }
+}
