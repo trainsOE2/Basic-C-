e8c6508f024a603432ce3d9035bf49add055d17a
Megha Singh <meghasingh.ms@gmail.com>
2018-01-09 15:22:16 +0530

    Update Program.cs
    static method "Parse" created

--- IntroToClasses/IntroToClasses/Program.cs
+++ IntroToClasses/IntroToClasses/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,13 +14,20 @@ namespace IntroToClasses
         {
             Console.WriteLine("Hi {0}, I am {1}", to, name);
         }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.name = str;
+
+            return person;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.name = "Megha";
+            var person = Person.Parse("Megha") ;
             person.Introduce("Mosh");
             Console.ReadKey();
 
