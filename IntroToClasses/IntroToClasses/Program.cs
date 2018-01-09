ebb46a4140e3a2de8c85ca5c0d1d626310ff50d4
Megha Singh <meghasingh.ms@gmail.com>
2018-01-09 13:33:42 +0530

    Add files via upload
    created new project "Intro to classes"

---
+++ IntroToClasses/IntroToClasses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToClasses
+{
+    class Person
+    {
+        public string name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.name = "Megha";
+            person.Introduce("Mosh");
+
+        }
+    }
+}
