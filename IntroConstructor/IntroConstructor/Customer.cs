28700acfd21ce489e9e5e55428f07dcd4f72db01
Megha Singh <meghasingh.ms@gmail.com>
2018-01-10 13:02:47 +0530

    Add files via upload

---
+++ IntroConstructor/IntroConstructor/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroConstructor
+{
+
+    class Customer
+    {
+        public string Name;
+        public int Id;
+
+        /*void Customer(string name)
+        {
+            this.Name = name;
+        }*/
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
