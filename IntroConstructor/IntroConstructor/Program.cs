ba136fa4467eb4cfc8a30208dd7ce9b61d43d44a
Megha Singh <meghasingh.ms@gmail.com>
2018-01-11 10:12:35 +0530

    Update Program.cs
    Passing values to Constructor

--- IntroConstructor/IntroConstructor/Program.cs
+++ IntroConstructor/IntroConstructor/Program.cs
@@ -11,7 +11,7 @@ namespace IntroConstructor
     {
         static void Main(string[] args)
         {
-            var customer = new Customer();
+            var customer = new Customer(1, "John");
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);
             Console.ReadKey();
