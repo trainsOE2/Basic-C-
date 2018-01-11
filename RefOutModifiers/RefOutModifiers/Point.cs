27f7b29306061e9d2d9d6706aa78d1edda83b3f7
Megha Singh <meghasingh.ms@gmail.com>
2018-01-11 11:40:13 +0530

    Add files via upload

---
+++ RefOutModifiers/RefOutModifiers/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefOutModifiers
+{
+    public class Point
+    {
+
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Move(Point newPoint)
+        {
+            this.x = newPoint.x;
+            this.y = newPoint.y;
+        }
+    }
+}
