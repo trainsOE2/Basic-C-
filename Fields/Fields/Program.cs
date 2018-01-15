69e1ebdf0737481972b86be7de5297246da23c0a
Megha Singh <meghasingh.ms@gmail.com>
2018-01-15 11:19:06 +0530

    Add files via upload
    Introduction to ReadOnly Fields

---
+++ Fields/Fields/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+            Console.WriteLine(customer.Orders.Count);
+            customer.Promote();
+            Console.WriteLine(customer.Orders.Count);
+            Console.ReadKey();
+        }
+    }
+}
