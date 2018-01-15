3edd631a927d8a9b0e5d1e6844ba44a7e4be36f8
Megha Singh <meghasingh.ms@gmail.com>
2018-01-15 14:35:57 +0530

    Add files via upload
    Encapsulation Introduction

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1996, 6, 20));
+            Console.WriteLine(person.GetBirthdate());
+            Console.ReadKey();
+        }
+    }
+}
