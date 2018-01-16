6f90ab664d90cb5e0e5847f8897e48fe88fd628e
Megha Singh <meghasingh.ms@gmail.com>
2018-01-16 14:40:04 +0530

    Add files via upload
    Introduction to Indexers

---
+++ Indexers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Mosh";
+            Console.WriteLine(cookie["name"]);
+            Console.ReadKey();
+        }
+    }
+}
