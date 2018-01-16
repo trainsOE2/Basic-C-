75992da6b834d96013acbacb19487d968f9e9d9f
Megha Singh <meghasingh.ms@gmail.com>
2018-01-16 10:44:20 +0530

    Add files via upload
    Introduction to Properties (using "get" and "set" methods)

---
+++ Properties/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class Person
+    {
+
+        public DateTime BirthDate { get; private set; }
+
+        public Person(DateTime birthdate)
+        {
+            BirthDate = birthdate;
+        }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - BirthDate;
+                var years = timeSpan.Days/365;
+
+                return years;
+            }
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1996, 6, 20));
+            Console.WriteLine(person.Age);
+            Console.ReadKey();
+        }
+    }
+}
