ed7c2675eb4a64bc8958eb72f1d700b844473f80
Megha Singh <meghasingh.ms@gmail.com>
2018-01-31 07:20:51 +0530

    file reading and writing.

---
+++ readWrite/readWrite/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.IO;
+
+namespace readWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            FileStream file = new FileStream("D:/C#/readWrite/data.txt", FileMode.OpenOrCreate);
+            StreamReader stream = new StreamReader(file);
+            line = stream.ReadLine();
+            while(line != null)
+            {
+                Console.WriteLine(line);
+                line = stream.ReadLine();
+            }
+
+            stream.Close();
+            Console.ReadKey();
+
+        }
+    }
+}
